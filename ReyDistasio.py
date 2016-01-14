ed97db8eca5ff9c3d959bdf50efd61ca8d8de3a4
leonardodistasio <leonardo.distasio@he-arc.ch>
2016-01-14 16:03:50 +0100

    Ajouter label et position

--- ReyDistasio.py
+++ ReyDistasio.py
@@ -16,29 +16,56 @@ pygame.display.set_caption('Exemple')
 screen = pygame.display.get_surface() 
 font = pygame.font.Font(None,30)
 
+try:
+    file = sys.argv[1]
+except:
+    file = None
+
+try:
+    gui = sys.argv[2]
+except:
+    gui = True
+
+class City:
+
+    def __init__(self, name, pos):
+        self.name = name
+        self.pos = pos
+
 def draw(positions):
-		screen.fill(0)
-		for pos in positions: 
-			pygame.draw.circle(screen,city_color,pos,city_radius)
-		text = font.render("Nombre: %i" % len(positions), True, font_color)
-		textRect = text.get_rect()
-		screen.blit(text, textRect)
-		pygame.display.flip()
+    screen.fill(0)
+    font = pygame.font.Font(None,20)
+
+    for pos in positions:
+        pygame.draw.circle(screen, city_color, pos.pos, city_radius)
+        screen.blit(font.render("%s %s" % (pos.name, pos.pos), True, font_color), pos.pos)
+
+    font = pygame.font.Font(None,30)
+    text = font.render("Nombre: %i" % len(positions), True, font_color)
+    textRect = text.get_rect()
+    screen.blit(text, textRect)
+    print(file)
+    pygame.display.flip()
 
 cities = []
 draw(cities)
 
 collecting = True
 
+def ga_solve(file=None, gui=True, maxtime=0):
+    print("coucou")
+
 while collecting:
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			sys.exit(0)
-		elif event.type == KEYDOWN and event.key == K_RETURN:
-			collecting = False
-		elif event.type == MOUSEBUTTONDOWN:
-			cities.append(pygame.mouse.get_pos())
-			draw(cities)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit(0)
+        elif event.type == KEYDOWN and event.key == K_RETURN:
+            collecting = False
+        elif event.type == MOUSEBUTTONDOWN:
+            name = "v"+str(len(cities))
+            newCity = City(name, pygame.mouse.get_pos())
+            cities.append(newCity)
+            draw(cities)
 			
 screen.fill(0)
 pygame.draw.lines(screen,city_color,True,cities)
@@ -46,6 +73,7 @@ text = font.render("Un chemin, pas le meilleur!", True, font_color)
 textRect = text.get_rect()
 screen.blit(text, textRect)
 pygame.display.flip()
+
 
 while True:
 	event = pygame.event.wait()
